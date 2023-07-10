60a42370171166f37dcad36932c4f5a0e4e7f75b
hayaboy <hayaboy@korea.ac.kr>
2023-07-10 17:42:46 +0900

    Initial commit

---
+++ src/main/java/com/spring/member/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.spring.member.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.member.vo.MemberVO;
+
+public interface MemberController {
+
+		public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		
+		public ModelAndView memberForm(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		public ModelAndView addMember(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		public ModelAndView modMemberForm(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		public ModelAndView delMember(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		
+		public ModelAndView updateMember(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		
+		
+		
+		public ModelAndView search(HttpServletRequest request, HttpServletResponse response) throws Exception;
+		
+		
+		
+
+}
